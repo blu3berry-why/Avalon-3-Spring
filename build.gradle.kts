de7142f15b0ef0f75d6a155cccb94accccbc7f82
blu3berry-why <59977342+blu3berry-why@users.noreply.github.com>
2023-05-29 01:36:27 +0200

    jacoco added

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.7.1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("jacoco")
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
+
 }
 
 group = "blu3berry.why"
@@ -48,4 +50,32 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+   /* extensions.configure(JacocoTaskExtension::class) {
+        excludes = listOf("jdk.internal.*") // Exclude JDK internal classes from coverage
+    }*/
 }
+
+jacoco {
+    toolVersion = "0.8.8"
+    reportsDirectory.set(layout.buildDirectory.dir("customJacocoReportDir"))
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(false)
+        csv.required.set(false)
+        html.outputLocation.set(layout.buildDirectory.dir("jacocoHtml"))
+    }
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
+}
+
+
+
+
+
